7f029de5df5cb87a69b6f23f696ac28969b2dbe3
cr554 <cody.roy@yale.edu>
2017-02-14 15:30:16 -0500

    added a functional list of all the good restaurants in the area using a listView and an array adapter

--- app/src/main/java/com/example/cr554/tourguide/Restaurant.java
+++ app/src/main/java/com/example/cr554/tourguide/Restaurant.java
@@ -5,8 +5,33 @@ package com.example.cr554.tourguide;
  */
 
 public class Restaurant {
-    private int starRating;
+    private String rating;
     private String name;
     private String typeOfFood;
     private String address;
+
+    public Restaurant(String i, String restaurantName, String food, String streetAddress){
+        //should use setters?
+        rating = i;
+        name = restaurantName;
+        typeOfFood = food;
+        address = streetAddress;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTypeOfFood() {
+        return typeOfFood;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
 }
