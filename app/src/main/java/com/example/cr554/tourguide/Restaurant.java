c5541f9df7f0ec532d81344f551ea645ec2a8079
cr554 <cody.roy@yale.edu>
2017-02-14 11:13:30 -0500

    added an intent and listener to switch from the main activity to the restaurant activity also set the skeleton for future UI / functionality updates

---
+++ app/src/main/java/com/example/cr554/tourguide/Restaurant.java
@@ -0,0 +1,12 @@
+package com.example.cr554.tourguide;
+
+/**
+ * Created by cr554 on 2/14/2017.
+ */
+
+public class Restaurant {
+    private int starRating;
+    private String name;
+    private String typeOfFood;
+    private String address;
+}
